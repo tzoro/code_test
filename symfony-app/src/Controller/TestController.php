d9303636efdf5cb84c7ccb8f97557ee98bc989ba
Tino <tino@klassapp.com>
2021-08-25 09:03:56 +0200

    Basic functionality

--- symfony-app/src/Controller/TestController.php
+++ symfony-app/src/Controller/TestController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpClient\HttpClient;
 
 class TestController extends AbstractController
 {
@@ -13,8 +14,43 @@ class TestController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
-        ]);
+      $t_count = (float) 0;
+      $p_count = (float) 0;
+      $n_count = (float) 0;
+      $p_score = (float) 0;
+      $n_score = (float) 0;
+      $t_score = (float) 0;
+      $diff    = (float) 0;
+
+      $client = HttpClient::create();
+
+      $response = $client->request('GET', 'https://api.github.com/repos/tzoro/code_test/issues', [
+          'auth_basic' => ['tzoro', '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'],
+      ])->toArray();
+
+      foreach ($response as $key => $value) {
+
+        if (str_contains($value['title'], 'rocks')) {
+          $p_count++;
+        } elseif (str_contains($value['title'], 'sucks')) {
+          $n_count++;
+        }
+
+        $t_count++;
+      }
+
+      $p_score = $p_count > 0 ? $p_count / $t_count : 0;
+      $n_score = $n_count > 0 ? $n_count / $t_count : 0;
+
+      if ($n_score > 0) {
+
+        $diff = $p_score - $n_score;
+        $t_score = $diff / $p_score;
+
+      }
+
+      return $this->render('test/index.html.twig', [
+          'controller_name' => 'TestController',
+      ]);
     }
 }
